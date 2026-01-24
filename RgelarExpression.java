6286c5e713c27491b136ffaba99a02dc40cd0443
Subham Pradhan <email_used_on_github>
2026-01-24 15:55:05 +0530

    rexhcvsy

--- RgelarExpression.java
+++ RgelarExpression.java
@@ -2,12 +2,12 @@
 public class RgelarExpression {
 
 	public static void main(String[] args) {
-		String input="abcdabefghabijkab";
-		String exp="ab.";
-		
-		int prev=input.length();
-		input=input.replaceAll(exp,"");
-		System.out.println((prev - input.length())/exp.length());
+		String input = "abcdabefghabijkab";
+		String exp = "ab.";
+
+		int prev = input.length();
+		input = input.replaceAll(exp, "");
+		System.out.println((prev - input.length()) / exp.length());
 	}
 
 }
